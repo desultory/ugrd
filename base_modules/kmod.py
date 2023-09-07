789591e9c7a2a45321858ed3a443e781ff122d8a
Zen <z@pyl.onl>
2023-09-07 10:50:53 -0500

    updated kmod module to pull kernel module metadata files
    Signed-off-by: Zen <z@pyl.onl>

--- base_modules/kmod.py
+++ base_modules/kmod.py
@@ -1,8 +1,11 @@
 __author__ = 'desultory'
 
-__version__ = '0.2.0'
+__version__ = '0.2.2'
 
 from subprocess import run
+
+MODULE_METADATA_FILES = ['modules.alias', 'modules.alias.bin', 'modules.builtin', 'modules.builtin.alias.bin', 'modules.builtin.bin', 'modules.builtin.modinfo',
+                         'modules.dep', 'modules.dep.bin', 'modules.devname', 'modules.order', 'modules.softdep', 'modules.symbols', 'modules.symbols.bin']
 
 
 def resolve_kmod(self, module_name):
@@ -48,6 +51,34 @@ def get_all_modules(self):
     return modules
 
 
+def get_module_metadata(self):
+    """
+    Gets all module metadata for the specified kernel version
+    """
+    from pathlib import Path
+
+    if 'kernel_version' not in self.config_dict:
+        self.logger.debug("Kernel version not specified, using current kernel")
+        cmd = run(['uname', '-r'], capture_output=True)
+        if cmd.returncode != 0:
+            self.logger.error('Failed to get kernel version')
+            self.logger.debug(f'Error: {cmd.stderr.decode("utf-8").strip()}')
+            return
+
+        kernel_version = cmd.stdout.decode('utf-8').strip()
+        self.logger.info(f'Using detected kernel version: {kernel_version}')
+    else:
+        kernel_version = self.config_dict['kernel_version']
+
+    module_path = Path('/lib/modules/') / kernel_version
+
+    for meta_file in MODULE_METADATA_FILES:
+        meta_file_path = module_path / meta_file
+
+        self.logger.debug("Adding kernel module metadata files to dependencies: %s", meta_file_path)
+        self.config_dict['dependencies'].append(meta_file_path)
+
+
 def fetch_modules(self):
     """
     Fetches all kernel modules
@@ -59,3 +90,5 @@ def fetch_modules(self):
             self.config_dict['dependencies'].append(module_path)
         else:
             self.logger.warning(f'Failed to add kernel module {module} to dependencies')
+
+    get_module_metadata(self)
