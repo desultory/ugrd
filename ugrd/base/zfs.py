ec25d39dbd7dfa60b9e8df0c10eb4b3ae6c2006c
Zen <z@pyl.onl>
2023-10-22 19:54:35 -0500

    fixed zfs example, added recovery bash to zmount commands
    Signed-off-by: Zen <z@pyl.onl>

--- ugrd/base/zfs.py
+++ ugrd/base/zfs.py
@@ -1,7 +1,12 @@
+__author__ = 'desultory'
+__version__ = '0.0.1'
 
 
 def mount_zfs_root(self):
     """
     Mounts the ZFS root pool
     """
-    return [f"zpool import -R /mnt/gentoo {self.config_dict['root_mount']['source']['label']}", 'zfs mount -a']
+    zpool_cmd = f"zpool import -R /mnt/root {self.config_dict['root_mount']['source']['label']} || (echo 'Failed to import ZFS root pool' && bash)"
+    zmount_cmd = "zfs mount -a || (echo 'Failed to mount ZFS root.' && bash)"
+
+    return [zpool_cmd, zmount_cmd]
