8debdf6f279eefcd271ba8e9c995aa12060e3828
Zen <z@pyl.onl>
2023-12-24 20:53:28 -0600

    update cmdline to dynamically run mount functions based on existing imports
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/base/cmdline.py
+++ src/ugrd/base/cmdline.py
@@ -1,5 +1,5 @@
 __author__ = 'desultory'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 
 def parse_cmdline(self) -> str:
@@ -8,12 +8,29 @@ def parse_cmdline(self) -> str:
             r"export CMDLINE_ROOT=$(grep -oP '(?<=root=)[^\s]+' /proc/cmdline)"]
 
 
+def refactor_mounts(self):
+    """
+    Refactors the imports['init_mount'] list to just be mount_cmdline_root.
+    Adds moved imports to self['_init_mount'] for later use.
+    """
+    self['_init_mount'] = []
+
+    for name in self['imports']['init_mount'].copy():
+        if name == 'mount_cmdline_root':
+            continue
+        if name not in self['_init_mount']:
+            self['_init_mount'].append(name)
+        self['imports']['init_mount'].remove(name)
+
+
 def mount_cmdline_root(self) -> str:
     """ Returns bash script to mount root partition based on /proc/cmdline """
     mount_dest = self['mounts']['root']['destination'] if not self.get('switch_root_target') else self['switch_root_target']
-    return ["echo 'Mounting root partition based on /proc/cmdline'",
-            f"mount $CMDLINE_ROOT {mount_dest} -o ro",
-            'if [ $? -ne 0 ]; then',
-            '    echo "Failed to mount the root parition using /proc/cmdline"',
-            '    mount_root',
-            'fi']
+    out_str = ["echo 'Mounting root partition based on /proc/cmdline'",
+               f"mount $CMDLINE_ROOT {mount_dest} -o ro",
+               'if [ $? -ne 0 ]; then',
+               '    echo "Failed to mount the root parition using /proc/cmdline"']
+    for name in self['_init_mount']:
+        out_str.append(f'    {name}')
+    out_str.append('fi')
+    return out_str
