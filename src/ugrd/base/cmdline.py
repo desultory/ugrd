84a612e7bd9cd82c243add5ffda90e8af68eeb8d
Zen <z@pyl.onl>
2023-12-24 19:02:11 -0600

    Updated cmdline to fall back on builtin mount if it fails
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/base/cmdline.py
+++ src/ugrd/base/cmdline.py
@@ -1,5 +1,5 @@
 __author__ = 'desultory'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 
 def parse_cmdline(self) -> str:
@@ -12,4 +12,8 @@ def mount_cmdline_root(self) -> str:
     """ Returns bash script to mount root partition based on /proc/cmdline """
     mount_dest = self['mounts']['root']['destination'] if not self.get('switch_root_target') else self['switch_root_target']
     return ["echo 'Mounting root partition based on /proc/cmdline'",
-            f"mount $CMDLINE_ROOT {mount_dest} -o ro"]
+            f"mount $CMDLINE_ROOT {mount_dest} -o ro",
+            'if [ $? -ne 0 ]; then',
+            '    echo "Failed to mount the root parition using /proc/cmdline"',
+            '    mount_root',
+            'fi']
