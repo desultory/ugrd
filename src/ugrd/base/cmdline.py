41c8cb4d42477a2b7413996313456b1f66a91e1e
Zen <z@pyl.onl>
2023-12-24 18:51:30 -0600

    updated cm config, added cmdline paramter to process the kernel commandline
    Signed-off-by: Zen <z@pyl.onl>

---
+++ src/ugrd/base/cmdline.py
@@ -0,0 +1,15 @@
+__author__ = 'desultory'
+__version__ = '0.1.0'
+
+
+def parse_cmdline(self) -> str:
+    """ Returns bash script to parse /proc/cmdline """
+    return ["echo 'Parsing /proc/cmdline'",
+            r"export CMDLINE_ROOT=$(grep -oP '(?<=root=)[^\s]+' /proc/cmdline)"]
+
+
+def mount_cmdline_root(self) -> str:
+    """ Returns bash script to mount root partition based on /proc/cmdline """
+    mount_dest = self['mounts']['root']['destination'] if not self.get('switch_root_target') else self['switch_root_target']
+    return ["echo 'Mounting root partition based on /proc/cmdline'",
+            f"mount $CMDLINE_ROOT {mount_dest} -o ro"]
