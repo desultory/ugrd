8e788b57d39a4ffe406e0ae571ef9e522c07bdb7
Zen <z@pyl.onl>
2023-12-28 18:23:31 -0600

    remove redundant functions in gpg, update smartcard module to use check_dict, and raise an exception if the sc_public_key is not defiend
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/crypto/gpg.py
+++ src/ugrd/crypto/gpg.py
@@ -1,21 +1,5 @@
 __author__ = 'desultory'
-__version__ = '0.5.0'
-
-
-from zenlib.util.check_dict import check_dict
-
-
-@check_dict('gpg_public_key', message="GPG public key file not specified, skipping")
-def fetch_keys(self) -> None:
-    """ Pulls the GPG keys into the initramfs. """
-    self.logger.info("Adding GPG public key file to dependencies: %s", self['gpg_public_key'])
-    self['dependencies'].append(self['gpg_public_key'])
-
-
-@check_dict('gpg_public_key', message="GPG public key file not specified, skipping")
-def import_keys(self) -> str:
-    """ Import GPG public keys. """
-    return f"gpg --import {self['gpg_public_key']}"
+__version__ = '0.6.0'
 
 
 def start_agent(self) -> str:
--- src/ugrd/crypto/smartcard.py
+++ src/ugrd/crypto/smartcard.py
@@ -1,29 +1,23 @@
 __author__ = 'desultory'
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 
+from zenlib.util.check_dict import check_dict
+
+
+@check_dict('sc_public_key', raise_exception=True, message="Smartcard public key file not specified (sc_public_key)")
 def fetch_keys(self) -> None:
-    """
-    Pulls the GPG keys into the initramfs.
-    """
-    if public_key := self.get('sc_public_key'):
-        self.logger.info("Adding GPG public key file to dependencies: %s", public_key)
-        self['dependencies'] = public_key
-    else:
-        self.logger.debug("No GPG public key specified, skipping")
+    self.logger.info("Adding GPG public key file to dependencies: %s", self['sc_public_key'])
+    self['dependencies'] = self['sc_public_key']
 
 
 def check_card(self) -> str:
-    """
-    Check if a smartcard is present.
-    """
+    """ Check if a smartcard is present. """
     return "gpg --card-status"
 
 
+@check_dict('sc_public_key', raise_exception=True, message="Smartcard public key file not specified (sc_public_key)")
 def import_keys(self) -> str:
-    """
-    Import GPG public keys.
-    """
-    if public_key := self.get('sc_public_key'):
-        return f"gpg --import {public_key}"
+    """ Import GPG public keys. """
+    return f"gpg --import {self['sc_public_key']}"
 
