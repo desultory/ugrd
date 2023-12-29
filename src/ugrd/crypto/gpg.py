cf2ea0c9d4cab5449727fb8fe383e689b305d5bd
Zen <z@pyl.onl>
2023-12-28 18:18:23 -0600

    update gpg module to use check_dict
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/crypto/gpg.py
+++ src/ugrd/crypto/gpg.py
@@ -1,29 +1,24 @@
 __author__ = 'desultory'
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 
+from zenlib.util.check_dict import check_dict
+
+
+@check_dict('gpg_public_key', message="GPG public key file not specified, skipping")
 def fetch_keys(self) -> None:
-    """
-    Pulls the GPG keys into the initramfs
-    """
-    if public_key := self.get('gpg_public_key'):
-        self.logger.info("Adding GPG public key file to dependencies: %s", public_key)
-        self['dependencies'].append(public_key)
-    else:
-        self.logger.debug("No GPG public key specified, skipping")
+    """ Pulls the GPG keys into the initramfs. """
+    self.logger.info("Adding GPG public key file to dependencies: %s", self['gpg_public_key'])
+    self['dependencies'].append(self['gpg_public_key'])
 
 
+@check_dict('gpg_public_key', message="GPG public key file not specified, skipping")
 def import_keys(self) -> str:
-    """
-    Import GPG public keys
-    """
-    if public_key := self.get('gpg_public_key'):
-        return f"gpg --import {public_key}"
+    """ Import GPG public keys. """
+    return f"gpg --import {self['gpg_public_key']}"
 
 
 def start_agent(self) -> str:
-    """
-    Start the GPG agent
-    """
+    """ Start the GPG agent. """
     args = " ".join(self['gpg_agent_args']) if self['gpg_agent_args'] else ""
     return f"gpg-agent {args}"
