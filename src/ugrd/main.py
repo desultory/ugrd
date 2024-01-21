122446f025d4f8531df0f9c343629f54e32166f4
Zen <z@pyl.onl>
2024-01-20 18:01:43 -0600

    simplify main using new zenlib
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/main.py
+++ src/ugrd/main.py
@@ -1,51 +1,31 @@
 #!/usr/bin/env python
 
 from ugrd.initramfs_generator import InitramfsGenerator
-from zenlib.util import init_logger, init_argparser, process_args
+from zenlib.util import get_kwargs_from_args, get_args_n_logger
 
 
 def main():
-    logger = init_logger(__package__)
-    argparser = init_argparser(prog=__package__, description='MicrogRAM disk initramfs generator')
+    arguments = [{'flags': ['--build-logging'], 'action': 'store_true', 'help': 'Enable additional build logging.'},
+                 {'flags': ['--no-build-logging'], 'action': 'store_true', 'help': 'Disable additional build logging.'},
+                 {'flags': ['-c', '--config'], 'action': 'store', 'help': 'Config file location.'},
+                 {'flags': ['--kver'], 'action': 'store', 'help': 'Set the kernel version.'},
+                 {'flags': ['--clean'], 'action': 'store_true', 'help': 'Enable build directory cleaning.'},
+                 {'flags': ['--no-clean'], 'action': 'store_true', 'help': 'Disable build directory cleaning.'},
+                 {'flags': ['--validate'], 'action': 'store_true', 'help': 'Enable config validation.'},
+                 {'flags': ['--no-validate'], 'action': 'store_true', 'help': 'Disable config validation.'},
+                 {'flags': ['--hostonly'], 'action': 'store_true', 'help': 'Enable hostonly mode, required for automatic kmod detection.'},
+                 {'flags': ['--no-hostonly'], 'action': 'store_true', 'help': 'Disable hostonly mode.'},
+                 {'flags': ['--lspci'], 'action': 'store_true', 'help': 'Use lspci to auto-detect kmods'},
+                 {'flags': ['--lsmod'], 'action': 'store_true', 'help': 'Use lsmod to auto-detect kmods'},
+                 {'flags': ['--firmware'], 'action': 'store_true', 'help': 'Include firmware files found with modinfo.'},
+                 {'flags': ['--no-firmware'], 'action': 'store_true', 'help': 'Exclude firmware files.'},
+                 {'flags': ['--autodetect-root'], 'action': 'store_true', 'help': 'Autodetect the root partition.'},
+                 {'flags': ['--no-autodetect-root'], 'action': 'store_true', 'help': 'Do not autodetect the root partition.'},
+                 {'flags': ['--print-config'], 'action': 'store_true', 'help': 'Print the final config dict.'},
+                 {'flags': ['output_file'], 'action': 'store', 'help': 'Output file location', 'nargs': '?'}]
 
-    argparser.add_argument('--build-logging', action='store_true', help='Enable additional build logging.')
-    argparser.add_argument('--no-build-logging', action='store_true', help='Disable additional build logging.')
-
-    # Add arguments for dracut compatibility
-    argparser.add_argument('-c', '--config', action='store', help='Config file location.')
-    argparser.add_argument('--kver', action='store', help='Set the kernel version.')
-
-    argparser.add_argument('--clean', action='store_true', help='Enable build directory cleaning.')
-    argparser.add_argument('--no-clean', action='store_true', help='Disable build directory cleaning.')
-
-    argparser.add_argument('--validate', action='store_true', help='Enable config validation.')
-    argparser.add_argument('--no-validate', action='store_true', help='Disable config validation.')
-
-    argparser.add_argument('--hostonly', action='store_true', help='Enable hostonly mode, required for automatic kmod detection.')
-    argparser.add_argument('--no-hostonly', action='store_true', help='Disable hostonly mode.')
-
-    # Add arguments for auto-kmod detection
-    argparser.add_argument('--lspci', action='store_true', help='Use lspci to auto-detect kmods')
-    argparser.add_argument('--lsmod', action='store_true', help='Use lsmod to auto-detect kmods')
-
-    # Add argument for firmware inclusion
-    argparser.add_argument('--firmware', action='store_true', help='Include firmware files found with modinfo.')
-    argparser.add_argument('--no-firmware', action='store_true', help='Exclude firmware files.')
-
-    # Add argument for autodecting the root partition
-    argparser.add_argument('--autodetect-root', action='store_true', help='Autodetect the root partition.')
-    argparser.add_argument('--no-autodetect-root', action='store_true', help='Do not autodetect the root partition.')
-
-    # Add the argument for the output file
-    argparser.add_argument('output_file', action='store', help='Output file location', nargs='?')
-
-    # Print the final config_dict
-    argparser.add_argument('--print-config', action='store_true', help='Print the final config dict.')
-
-    args = process_args(argparser, logger=logger)
-
-    # Pass the logger to the generator
-    kwargs = {'logger': logger}
+    args, logger = get_args_n_logger(package=__package__, description='MicrogRAM disk initramfs generator', arguments=arguments)
+    kwargs = get_kwargs_from_args(args, logger=logger)
 
     # Set config toggles
     for toggle in ['validate', 'hostonly', 'firmware', 'autodetect_root', 'clean', 'build_logging']:
