a9ba8c1ba0fea357d15ae1f68beebc863d09492b
Zen <z@pyl.onl>
2024-05-12 20:04:51 -0500

    improve spacing
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/crypto/gpg.py
+++ src/ugrd/crypto/gpg.py
@@ -1,9 +1,9 @@
 __author__ = 'desultory'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 
 def start_agent(self) -> str:
     """ Start the GPG agent. """
-    args = " ".join(self['gpg_agent_args']) if self['gpg_agent_args'] else ""
-    return f"gpg-agent {args}"
+    args = ' ' + " ".join(self['gpg_agent_args']) if self['gpg_agent_args'] else ""
+    return f"gpg-agent  {args}"
 
