a55722b523416d568bcea245423df6f52c73ec5d
Zen <z@pyl.onl>
2024-05-27 13:12:31 -0500

    improve file name resolution
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/fs/cpio.py
+++ src/ugrd/fs/cpio.py
@@ -1,5 +1,5 @@
 __author__ = 'desultory'
-__version__ = '2.8.1'
+__version__ = '2.8.2'
 
 
 from pycpio import PyCPIO
@@ -69,6 +69,11 @@ def _process_out_file(self, out_file):
         self.logger.info("Specified out_file is a directory, setting out_dir: %s" % out_file)
         self['out_dir'] = out_file
         return
+
+    if Path(out_file).parent.is_dir():
+        self['out_dir'] = Path(out_file).parent
+        self.logger.info("Resolved out_dir to: %s" % self['out_dir'])
+        out_file = Path(out_file).name
 
     if out_file.startswith('./'):
         from pathlib import Path
