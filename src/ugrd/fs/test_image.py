e5ba61035f41ec865abd6d4ff9770e0b0db0b7c7
Zen <z@pyl.onl>
2024-07-18 18:17:37 -0500

    standardize banner
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/fs/test_image.py
+++ src/ugrd/fs/test_image.py
@@ -1,11 +1,6 @@
 __version__ = "0.1.0"
 
 
-def test_init(self):
-    """ Test init function. """
-    return "echo DONE"
-
-
 def make_test_image(self):
     """ Creates a test image from the build dir """
 
