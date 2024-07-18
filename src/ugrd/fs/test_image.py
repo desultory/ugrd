0f16b3f727cfcacd5d96e7da69cb7f6547636f9c
Zen <z@pyl.onl>
2024-07-18 15:44:52 -0500

    add support for btrfs images, add system root uuid to the image
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/fs/test_image.py
+++ src/ugrd/fs/test_image.py
@@ -1,3 +1,6 @@
+__version__ = "0.1.0"
+
+
 def test_init(self):
     """ Test init function. """
     return "echo DONE"
@@ -13,4 +16,13 @@ def make_test_image(self):
         self.logger.info("Creating test image file: %s" % self._archive_out_path)
         f.write(b"\0" * self.test_image_size * 2 ** 20)
 
-    self._run(['mkfs.ext4', '-d', self.build_dir.resolve(), self._archive_out_path])
+    rootfs_uuid = self['mounts']['root']['uuid']
+    rootfs_type = self['mounts']['root']['type']
+
+    if rootfs_type == 'ext4':
+        self._run(['mkfs', '-t', rootfs_type, '-d', self.build_dir.resolve(), '-U', rootfs_uuid, '-F', self._archive_out_path])
+    elif rootfs_type == 'btrfs':
+        self._run(['mkfs', '-t', rootfs_type, '--rootdir', self.build_dir.resolve(), '-U', rootfs_uuid, self._archive_out_path])
+    else:
+        raise Exception("Unsupported rootfs type: %s" % rootfs_type)
+
--- src/ugrd/initramfs_tester.py
+++ src/ugrd/initramfs_tester.py
@@ -6,10 +6,10 @@ from ugrd.initramfs_generator import InitramfsGenerator
 
 @loggify
 class InitramfsTester:
-    def __init__(self, initrd_generator, test_dir="./test_fs", *args, **kwargs):
+    def __init__(self, initrd_generator, test_dir="/tmp/initramfs_test_rootfs", *args, **kwargs):
         self.initrd_generator = initrd_generator
 
-        self.target_fs = InitramfsGenerator(logger=self.logger, build_dir=test_dir, config=None, modules='ugrd.fs.test_image', NO_BASE=True, out_dir=initrd_generator.out_dir, out_file='ugrd-test-rootfs')
+        self.target_fs = InitramfsGenerator(logger=self.logger, validate=False, build_dir=test_dir, config=None, modules='ugrd.fs.test_image', NO_BASE=True, out_dir=initrd_generator.out_dir, out_file='ugrd-test-rootfs', mounts=initrd_generator['mounts'])
 
     def test(self):
         self.target_fs.build()
