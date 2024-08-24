406b04e1b4fc8fbe03a2fb715faa06f3c9c4220f
Zen <z@pyl.onl>
2024-08-24 12:32:56 -0500

    add fakeudev
    Signed-off-by: Zen <z@pyl.onl>

---
+++ src/ugrd/fs/fakeudev.py
@@ -0,0 +1,22 @@
+"""
+UDEV is amazing and well desinged software.
+
+In order to make systemd not time out on boot, we need to fake it.
+To do this, we simply need to read /sys/block/dm-*/uevent,
+We can then write 'E:DM_UDEV_PRIMARY_SOURCE_FLAG=1\n' to each:
+    /run/udev/data/b<MAJOR>:<MINOR>.
+This will make systemd think that udev is working and not time out.
+"""
+
+
+def fake_dm_udev(self):
+    """
+    returns a bash script to fake udev for dm devices.
+    calls _make_fake_dm_udev_db for each dm device.
+    """
+    return ['for dm in /sys/block/dm-*; do',
+            '    source "${dm}/uevent"',
+            '    einfo "Faking udev for ${DEVNAME}"',
+            '    udev_db_file="/run/udev/data/b${MAJOR}:${MINOR}"',
+            "    echo -e 'E:DM_UDEV_PRIMARY_SOURCE_FLAG=1\n' > ${udev_db_file}",
+            'done']
