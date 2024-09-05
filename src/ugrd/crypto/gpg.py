b39e1d1df691b180083ad32f82dd6e5bbd0a3fe2
Zen <z@pyl.onl>
2024-09-05 17:33:15 -0500

    combine stderr into stdout for log statments
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/crypto/gpg.py
+++ src/ugrd/crypto/gpg.py
@@ -5,5 +5,5 @@ __version__ = '1.1.2'
 def start_agent(self) -> str:
     """ Start the GPG agent. """
     args = (' ' + " ".join(self['gpg_agent_args'])) if self['gpg_agent_args'] else ""
-    return f'einfo "Staring GPG agent: $(gpg-agent{args})"'
+    return f'einfo "Staring GPG agent: $(gpg-agent{args} 2>&1)"'
 
