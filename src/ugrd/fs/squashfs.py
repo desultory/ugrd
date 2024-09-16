554257b4a7fb6b3e275692d155a6472fb82237cb
Zen <z@pyl.onl>
2024-09-15 19:53:46 -0500

    raise an exception if the squashfs_image var is not set
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/fs/squashfs.py
+++ src/ugrd/fs/squashfs.py
@@ -1,3 +1,7 @@
+from zenlib.util import unset
+
+
+@unset("squashfs_image", "squashfs_image must be set to the path of the squashfs image to mount.", raise_exception=True)
 def mount_squashfs(self):
     """
     Returns bash lines to mount squashfs image.
