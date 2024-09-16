cbf29f7e07d7df7e8f7cac78ebfc3df247d0fc26
Zen <z@pyl.onl>
2024-09-15 19:29:19 -0500

    add base squashfs/overlayfs module
    Signed-off-by: Zen <z@pyl.onl>

---
+++ src/ugrd/fs/squashfs.py
@@ -0,0 +1,11 @@
+def mount_squashfs(self):
+    """
+    Returns bash lines to mount squashfs image.
+    Creates /run/squashfs directory to mount squashfs image.
+    Creates /run/upperdir and /run/workdir directories for overlayfs.
+    """
+    return ["mkdir -p /run/squashfs",
+            f"mount -t squashfs -o loop {self.squashfs_image} /run/squashfs",
+            "mkdir -p /run/upperdir",
+            "mkdir -p /run/workdir",
+            f"mount -t overlay overlay -o lowerdir=/run/squashfs,upperdir=/run/upperdir,workdir=/run/workdir {self.mounts['root']['destination']}"]
