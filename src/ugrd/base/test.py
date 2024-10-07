f66f848c237310ec791bcb6650b1be2e2ba52547
Zen <z@pyl.onl>
2024-10-07 15:28:15 -0500

    actually fix tests (maybe)
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/base/test.py
+++ src/ugrd/base/test.py
@@ -26,33 +26,10 @@ def init_test_vars(self):
         self['test_flag'] = uuid4()
 
 
-def make_test_image(self):
-    """ Creates a new initramfs generator to creaate the test image """
-    from ugrd.initramfs_generator import InitramfsGenerator
-
-    kwargs = {'logger': self.logger,
-              'validate': False,
-              'NO_BASE': True,
-              'config': None,
-              'modules': 'ugrd.fs.test_image',
-              'out_file': self['test_rootfs_name'],
-              'build_dir': self['test_rootfs_build_dir'],
-              **{key: self[key] for key in COPY_CONFIG}}
-
-    target_fs = InitramfsGenerator(**kwargs)
-    try:
-        target_fs.build()
-    except (FileNotFoundError, RuntimeError) as e:
-        self.logger.error("Test image configuration:\n%s", target_fs)
-        raise RuntimeError("Failed to build test rootfs: %s" % e)
-
-    self['_test_rootfs'] = target_fs
-
-
-def _get_qemu_cmd_args(self):
+def _get_qemu_cmd_args(self, test_image):
     """ Gets the qemu command from the configuration """
     test_initrd = self._archive_out_path
-    test_rootfs = self['_test_rootfs']['_archive_out_path']
+    test_rootfs = test_image['_archive_out_path']
     qemu_args = {'-m': self['test_memory'],
                  '-cpu': self['test_cpu'],
                  '-kernel': self['test_kernel'],
@@ -71,12 +48,37 @@ def _get_qemu_cmd_args(self):
     return arglist
 
 
+def make_test_image(self):
+    """ Creates a new initramfs generator to create the test image """
+    from ugrd.initramfs_generator import InitramfsGenerator
+
+    kwargs = {'logger': self.logger,
+              'validate': False,
+              'NO_BASE': True,
+              'config': None,
+              'modules': 'ugrd.fs.test_image',
+              'out_file': self['test_rootfs_name'],
+              'build_dir': self['test_rootfs_build_dir'],
+              **{key: self[key] for key in COPY_CONFIG}}
+
+    target_fs = InitramfsGenerator(**kwargs)
+    try:
+        target_fs.build()
+    except (FileNotFoundError, RuntimeError) as e:
+        self.logger.error("Test image configuration:\n%s", target_fs)
+        raise RuntimeError("Failed to build test rootfs: %s" % e)
+
+    return target_fs
+
+
 def test_image(self):
-    qemu_cmd = _get_qemu_cmd_args(self)
+    image = make_test_image(self)
+    qemu_cmd = _get_qemu_cmd_args(self, image)
+
     self.logger.info("Testing initramfs image: %s", self['_archive_out_path'])
-    self.logger.debug("Test config:\n%s", self._test_rootfs)
+    self.logger.debug("Test config:\n%s", image)
     self.logger.info("Test kernel: %s", self['test_kernel'])
-    self.logger.info("Test rootfs: %s", self['_test_rootfs']['_archive_out_path'])
+    self.logger.info("Test rootfs: %s", image['_archive_out_path'])
     self.logger.info("Test flag: %s", self['test_flag'])
     self.logger.info("QEMU command: %s", ' '.join([str(arg) for arg in qemu_cmd]))
 
