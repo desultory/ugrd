2df9e0a25ceebd4113c89092f29e504a87b6054f
Zen <z@pyl.onl>
2024-10-13 15:18:21 -0500

    black format, don't _log_init by default
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/fs/cpio.py
+++ src/ugrd/fs/cpio.py
@@ -1,41 +1,41 @@
-__author__ = 'desultory'
-__version__ = '3.4.3'
+__author__ = "desultory"
+__version__ = "3.5.0"
 
 
 from zenlib.util import contains
 
 
-@contains('check_cpio')
+@contains("check_cpio")
 def check_cpio_deps(self) -> None:
-    """ Checks that all dependenceis are in the generated CPIO file. """
-    for dep in self['dependencies']:
+    """Checks that all dependenceis are in the generated CPIO file."""
+    for dep in self["dependencies"]:
         _check_in_cpio(self, dep)
     return "All dependencies found in CPIO."
 
 
-@contains('check_cpio')
+@contains("check_cpio")
 def check_cpio_funcs(self) -> None:
-    """ Checks that all included functions are in the profile included in the generated CPIO file. """
-    bash_func_names = [func + '() {' for func in self.included_functions]
-    _check_in_cpio(self, 'etc/profile', bash_func_names)
+    """Checks that all included functions are in the profile included in the generated CPIO file."""
+    bash_func_names = [func + "() {" for func in self.included_functions]
+    _check_in_cpio(self, "etc/profile", bash_func_names)
     return "All functions found in CPIO."
 
 
-@contains('check_in_cpio')
-@contains('check_cpio')
+@contains("check_in_cpio")
+@contains("check_cpio")
 def check_in_cpio(self) -> None:
-    """ Checks that all required files and lines are in the generated CPIO file. """
-    for file, lines in self['check_in_cpio'].items():
+    """Checks that all required files and lines are in the generated CPIO file."""
+    for file, lines in self["check_in_cpio"].items():
         _check_in_cpio(self, file, lines)
     return "All files and lines found in CPIO."
 
 
 def _check_in_cpio(self, file, lines=[]):
-    """ Checks that the file is in the CPIO archive, and it contains the specified lines. """
+    """Checks that the file is in the CPIO archive, and it contains the specified lines."""
     cpio = self._cpio_archive
-    file = str(file).lstrip('/')  # Normalize as it may be a path
+    file = str(file).lstrip("/")  # Normalize as it may be a path
     if file not in cpio.entries:
-        self.logger.warning("CPIO entries:\n%s" % '\n'.join(cpio.entries.keys()))
+        self.logger.warning("CPIO entries:\n%s" % "\n".join(cpio.entries.keys()))
         raise FileNotFoundError("File not found in CPIO: %s" % file)
     else:
         self.logger.debug("File found in CPIO: %s" % file)
@@ -50,20 +50,22 @@ def _check_in_cpio(self, file, lines=[]):
 
 
 def get_archive_path(self) -> str:
-    """ Determines the filename for the output CPIO archive based on the current configuration. """
-    if out_file := self.get('out_file'):
+    """Determines the filename for the output CPIO archive based on the current configuration."""
+    if out_file := self.get("out_file"):
         self.logger.info("Using specified out_file: %s" % out_file)
     else:
-        if self.get('kmod_init'):
+        if self.get("kmod_init"):
             out_file = f"ugrd-{self['kernel_version']}.cpio"
         else:
             out_file = "ugrd.cpio"
 
-        if compression_type := self['cpio_compression']:
-            if compression_type.lower() != 'false':  # The variable is a string, so we need to check for the string 'false'
+        if compression_type := self["cpio_compression"]:
+            if (
+                compression_type.lower() != "false"
+            ):  # The variable is a string, so we need to check for the string 'false'
                 out_file += f".{compression_type}"
 
-    self['_archive_out_path'] = self.out_dir / out_file
+    self["_archive_out_path"] = self.out_dir / out_file
 
 
 def make_cpio(self) -> None:
@@ -74,24 +76,24 @@ def make_cpio(self) -> None:
     Raises FileNotFoundError if the output directory does not exist.
     """
     cpio = self._cpio_archive
-    cpio.append_recursive(self._get_build_path('/'), relative=True)
+    cpio.append_recursive(self._get_build_path("/"), relative=True)
 
-    if self.get('mknod_cpio'):
-        for node in self['nodes'].values():
+    if self.get("mknod_cpio"):
+        for node in self["nodes"].values():
             self.logger.debug("Adding CPIO node: %s" % node)
-            cpio.add_chardev(name=node['path'], mode=node['mode'], major=node['major'], minor=node['minor'])
+            cpio.add_chardev(name=node["path"], mode=node["mode"], major=node["major"], minor=node["minor"])
 
-    out_cpio = self['_archive_out_path']
+    out_cpio = self["_archive_out_path"]
     if not out_cpio.parent.exists():
         self._mkdir(out_cpio.parent, resolve_build=False)
 
     if out_cpio.exists():
-        if self['cpio_rotate']:
+        if self["cpio_rotate"]:
             self._rotate_old(out_cpio)
-        elif self['clean']:
+        elif self["clean"]:
             self.logger.warning("Removing existing file: %s" % out_cpio)
             out_cpio.unlink()
         else:
             raise FileExistsError("File already exists, and cleaning/rotation are disabled: %s" % out_cpio)
 
-    cpio.write_cpio_file(out_cpio, compression=self['cpio_compression'], _log_bump=-10, _log_init=False)
+    cpio.write_cpio_file(out_cpio, compression=self["cpio_compression"], _log_bump=-10)
