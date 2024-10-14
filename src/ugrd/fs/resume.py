697e8a7c0f6c16a2216028322a2ebf6e00e73355
Zen <z@pyl.onl>
2024-10-14 17:23:52 -0500

    improve resume flow
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/fs/resume.py
+++ src/ugrd/fs/resume.py
@@ -1,18 +1,22 @@
-__version__ = "0.1.0"
+__version__ = "0.2.0"
+
 
 def handle_resume(self) -> None:
-    """Returns a bash script handling resume if specified.
+    """Returns a bash script handling resume from hibernation.
     Checks that /sys/power/resume is writable, and if resume= is set, if so,
-    it checks ifthe specified device exists, then echo's the resume device to /sys/power/resume."""
+    it checks ifthe specified device exists, then echo's the resume device to /sys/power/resume.
+    In the event of failure, it prints an error message and a list of block devices.
+    """
     return [
         'if [ -n "$(readvar resume)" ] && [ -w /sys/power/resume ]; then',
         '    if [ -e "$(readvar resume)" ]; then',
         '        einfo "Resuming from: $(readvar resume)"',
         "        readvar resume > /sys/power/resume",
-        '        rd_fail "Failed to resume from $(readvar resume)"',
+        '        eerror "Failed to resume from $(readvar resume)"',
         "    else",
-        '        ewarn "Resume device not found: $(readvar resume)"',
-        '        prompt_user "Press enter to continue booting."',
+        '        eerror "Resume device not found: $(readvar resume)"',
         "    fi",
+        r'    eerror "Block devices:\n$(blkid)"',
+        '    prompt_user "Press enter to continue booting."',
         "fi",
     ]
