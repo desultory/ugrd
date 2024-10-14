dc2d325ce2682406df949c0a374e9cb025d5ef5d
Zen <z@pyl.onl>
2024-10-14 18:34:51 -0500

    add noresume arg
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/fs/resume.py
+++ src/ugrd/fs/resume.py
@@ -1,14 +1,14 @@
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 
 def handle_resume(self) -> None:
     """Returns a bash script handling resume from hibernation.
-    Checks that /sys/power/resume is writable, and if resume= is set, if so,
+    Checks that /sys/power/resume is writable, resume= is set, and noresume is not set, if so,
     it checks ifthe specified device exists, then echo's the resume device to /sys/power/resume.
     In the event of failure, it prints an error message and a list of block devices.
     """
     out_str = [
-        'if [ -n "$(readvar resume)" ] && [ -w /sys/power/resume ]; then',
+        'if ! check_var noresume && [ -n "$(readvar resume)" ] && [ -w /sys/power/resume ]; then',
         '    if [ -e "$(readvar resume)" ]; then',
         '        einfo "Resuming from: $(readvar resume)"',
         "        readvar resume > /sys/power/resume",
@@ -20,7 +20,7 @@ def handle_resume(self) -> None:
     if self["safe_resume"]:
         out_str += [
             '    eerror "If you wish to continue booting, remove the resume= kernel parameter."',
-            '''    eerror " or run 'setvar resume ""' to clear the resume device."''',
+            '''    eerror " or run 'setvar noresume 1' to skip resuming."''',
             '    rd_fail "Failed to resume from $(readvar resume)."',
         ]
     else:
