8d25e6cd4b72bddf1818145f384af5686b65a21c
Zen <z@pyl.onl>
2024-11-10 15:32:03 -0600

    add the zfsutil mount option to zfs mounts
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/fs/mounts.py
+++ src/ugrd/fs/mounts.py
@@ -111,6 +111,7 @@ def _process_mount(self, mount_name: str, mount_config, mount_class="mounts") ->
             if "ugrd.fs.zfs" not in self["modules"]:
                 self.logger.info("Auto-enabling module: zfs")
                 self["modules"] = "ugrd.fs.zfs"
+                mount_config["options"].add("zfsutil")
         elif mount_type not in ["proc", "sysfs", "devtmpfs", "tmpfs", "devpts"]:
             self.logger.warning("Unknown mount type: %s" % mount_type)
 
