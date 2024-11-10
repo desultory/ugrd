612f65560eda44f1eafd584a14081dfdacee531f
Zen <z@pyl.onl>
2024-11-10 17:18:15 -0600

    disable udev timeout scanning zfs pools
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/fs/zfs.py
+++ src/ugrd/fs/zfs.py
@@ -1,7 +1,8 @@
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 
 def zpool_import(self) -> list[str]:
     """ Returns bash lines to import all ZFS pools """
     return ["edebug 'Importing all ZFS pools'",
+            'export ZPOOL_IMPORT_UDEV_TIMEOUT_MS=0',  # Disable udev timeout
             'einfo "$(zpool import -a)"']
