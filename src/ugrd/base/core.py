5b0306eae2207941421b0db8f931cbb05f8b124e
Zen <z@pyl.onl>
2024-11-18 13:49:10 -0600

    assume any "out_file" with a / in it should not be under a tmpdir
    previously, ./ had to be explictly passed for the out_dir to be updated,
    and the tmpdir not be used. This broke running make install from the
    /usr/src/linux dir, as it passes a path like arch/boot/initrd which then
    got interpreted to be a new out_dir, under the tmpdir.
    
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/base/core.py
+++ src/ugrd/base/core.py
@@ -1,5 +1,5 @@
 __author__ = "desultory"
-__version__ = "3.10.0"
+__version__ = "3.10.1"
 
 from pathlib import Path
 from typing import Union
@@ -186,8 +186,9 @@ def find_libgcc(self) -> None:
 def _process_out_file(self, out_file: str) -> None:
     """Processes the out_file.
     If set to the current directory, resolves and sets the out_dir to the current directory.
-    If it starts with './', resolves it to the current directory.
-    If it is a directory, sets the out_dir to the directory.
+    If a '/' is present, resolves the path and sets the out_dir to the parent directory.
+    If out_file is a directory, sets the out_dir to the directory, stops processing.
+
     If it's an absolute path, sets the out_dir to the parent directory.
     """
     out_file = str(out_file)
@@ -197,10 +198,9 @@ def _process_out_file(self, out_file: str) -> None:
         self["out_dir"] = current_dir
         return
 
-    if out_file.startswith("./"):
-        cwd = Path(".").resolve()
-        self.logger.info("Resolved relative out_file path: %s" % cwd)
-        out_file = cwd / out_file[2:]
+    if "/" in out_file: # If the out_file contains a path, resolve it
+        out_file = Path(out_file).resolve()
+        self.logger.info("Resolved relative output path: %s" % out_file)
     else:
         out_file = Path(out_file)
 
@@ -209,7 +209,7 @@ def _process_out_file(self, out_file: str) -> None:
         self["out_dir"] = out_file
         return
 
-    if out_file.parent.is_dir() and str(out_file.parent) != ".":
+    if str(out_file.parent) != ".":  # If the parent isn't the curent dir, set the out_dir to the parent
         self["out_dir"] = out_file.parent
         self.logger.info("Resolved out_dir to: %s" % self["out_dir"])
         out_file = out_file.name
