dce9c0c9e82d435f5510caf4ae0d606be3666066
truffle <truffle074@gmail.com>
2024-12-11 16:42:59 +1100

    more safety checks and features
    currently it's more of a rearrangment of the original code, with some
    added safety/integrity checks
    `resume` is now a separate function, the plan is to have the code for
    actually resuming within this function and code for locating the device
    outside.
    in the more general case, if `late_resume` is enabled, the initram will
    check at both the `init_early` and again at the `init_premount` stage
    for the presence of the resume device. Once host-only is implemented
    then it'll be able to determine at build time which state it'll need to
    check at.
    For safety checks, currently it checks whether the device is a
    valid block device, whether resume has already been attempted previous
    (based on the content of `/sys/power/resume`) & whether there are are
    any other mounted block devices.

--- src/ugrd/fs/resume.py
+++ src/ugrd/fs/resume.py
@@ -1,8 +1,8 @@
 __version__ = "0.4.2"
 
-from zenlib.util import contains, unset
+from zenlib.util import contains
 
-def _resume(self) -> None:
+def resume(self) -> None:
     """Returns a shell script handling resume from hibernation.
     Checks that /sys/power/resume is writable, resume= is set, and noresume is not set, if so,
     checks if UUID= or PARTUUID= or LABEL= is in the resume var,
@@ -17,32 +17,48 @@ def _resume(self) -> None:
     Distinguising between a fresh boot and missing/borked hibernation image is not possible at run time.
     """
     return [
-        "resumeval=$(readvar resume)",  # read the cmdline resume var
-        'if ! check_var noresume && [ -n "$resumeval" ] && [ -w /sys/power/resume ]; then',
-        '    if echo "$resumeval" | grep -q -E "^PARTUUID=|^UUID="; then',  # resolve partuuid to device
-        '        resume=$(blkid -t "$resumeval" -o device)',
-        "    else",
-        "        resume=$resumeval",
-        "    fi",
-        '    if [ -e "$resume" ]; then',  # Check if the resume device exists
-        '        einfo "Resuming from: $resume"',
-        '        printf "%s" "$resume" > /sys/power/resume',  # Attempt to resume
-        '        ewarn "Failed to resume from: $resume"',
-        "    else",
-        '        ewarn "Resume device not found: $resume)"',  # Warn if the resume device does not exist
-        r'        eerror "Block devices:\n$(blkid)"',
-        '        eerror "If you wish to continue booting, remove the resume= kernel parameter."',
-        '''        eerror " or run 'setvar noresume 1' from the recovery shell to skip resuming."''',
-        '        rd_fail "Failed to resume from $(readvar resume)."',
-        "    fi",
-        "fi",
+        # Check resume support
+        '[ -n "$1" ] || (ewarn "No device?" ; return 1)',
+        '[ -w /sys/power/resume ] || (ewarn "Kernel does not support resume!" ; return 1)',
+        '[[ ! "$(cat /sys/power/resume)" == "0:0" ]] || ewarn "/sys/power/resume not empty, resume has already been attempted!"',
+
+        # Safety checks
+        'if [ ! -z $(lsblk -Q MOUNTPOINT)] ; then',
+        r'    eerror "Cannot safely resume with mounted block devices:\n$(lsblk -Q MOUNTPOINT -no PATH)"',
+        '    return 1',
+        'fi',
+
+        '[ -b "$1" ] || (ewarn "\'$1\' is not a valid block device!" ; return 1)',
+
+        'einfo "Attempting resume from: $1"',
+        'echo -n "$1" > /sys/power/resume',
+        'einfo "No image on $resume"',
+        'return 0',
     ]
 
-@unset('late_resume')
-def handle_resume(self) -> None:
-    return _resume(self)
+def handle_early_resume(self) -> None:
+    return [
+        'resumeval=$(readvar resume)',
+        'if ! check_var noresume && [ -n "$resumeval" ] ; then',
+        '    if echo "$resumeval" | grep -qE "^PARTUUID=|^UUID="; then',
+        '        resume=$(blkid -t "$resumeval" -o device)',
+        '    else',
+        '        resume="$resumeval"',
+        '    fi',
+
+        '    if [ -n $resume ] ; then',
+        '        if ! resume "$resume" ; then',
+        '            eerror "If you wish to continue booting, remove the resume= kernel parameter."',
+        '''             eerror " or run 'setvar noresume 1' from the recovery shell to skip resuming."''',
+        '            rd_fail "Failed to resume from $(readvar resume)."',
+        '        fi',
+        '    else',
+        '        einfo "Resume device \'$resumeval\' not found"',
+        '    fi',
+        'fi',
+    ]
 
 @contains('late_resume')
 def handle_late_resume(self) -> None:
-    self.logger.warning("WARNING: Late resume enabled, can cause instability if not used with caution. Highly recommended to read configuration documentation before use.")
-    return _resume(self)
+    self.logger.warning("[late_resume] enabled, this can result in data loss if filesystems are modified before resuming. Read the docs for more info.")
+    return handle_early_resume(self)  # At the moment it's the same code but delayed, will change when more features are added
