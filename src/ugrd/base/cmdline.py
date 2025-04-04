fef2a055f192011681118fb9a02dfbf0b9efcb67
Zen <z@pyl.onl>
2025-04-04 14:15:09 -0500

    use option=value values from the env instad of reading the cmdline
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/base/cmdline.py
+++ src/ugrd/base/cmdline.py
@@ -1,5 +1,5 @@
 __author__ = "desultory"
-__version__ = "3.1.1"
+__version__ = "4.0.0"
 
 from importlib.metadata import PackageNotFoundError, version
 
@@ -7,6 +7,8 @@ from importlib.metadata import PackageNotFoundError, version
 def parse_cmdline_bool(self) -> str:
     """Returns a shell script to parse a boolean value from /proc/cmdline
     The only argument is the name of the variable to be read/set
+
+    If the boolean is present in /proc/cmdline, the variable is set to 1.
     """
     return r"""
     edebug "Parsing cmdline bool: $1"
@@ -15,21 +17,35 @@ def parse_cmdline_bool(self) -> str:
 
 
 def parse_cmdline_str(self) -> str:
-    """Returns a shell script to parse a string value from /proc/cmdline
+    """Returns a shell script to parse a string value from the environment.
+    The kernel should pass option=value pairs from the cmdline to the environment of the initramfs init.
+
     The only argument is the name of the variable to be read/set
+
+    Checks if that variable is set in the environment, and if so, sets it to the value.
     """
     return r"""
     edebug "Parsing cmdline string: $1"
-    val=$(grep -oP "(?<=$1=)[^\s]+" /proc/cmdline)
+
+    val=$(eval printf '%s' "\$$1")  # Get the value of the variable
+
     if [ -n "$val" ]; then
-        edebug "Parsed $1: $val"
+        edebug "[$1] Got cmdline string: ${val}"
         setvar "$1" "$val"
     fi
     """
 
 
 def parse_cmdline(self) -> str:
-    """Returns shell script to parse /proc/cmdline"""
+    """Returns shell script to parse /proc/cmdline
+
+    The portion before '--', if any, is the initramfs cmdline;
+    the portion after '--' are arguments for the system init.
+
+    The kernel will pass option=value pairs to the environment of the initramfs init.
+    These can be processed into variables for ugrd.
+    Named args are treated like boolean values, and are set to 1 if present.
+    """
     return rf"""
     cmdline=$(awk -F '--' '{{print $1}}' /proc/cmdline)  # Get everything before '--'
     setvar INIT_ARGS "$(awk -F '--' '{{print $2}}' /proc/cmdline)"  # Get everything after '--'
