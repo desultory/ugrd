d29d610923e31860a37d1166a793602bdfe270e2
Zen <z@pyl.onl>
2025-04-12 13:20:45 -0500

    handle missing f2fs modules for tests
    Signed-off-by: Zen <z@pyl.onl>

--- tests/test_filesystems.py
+++ tests/test_filesystems.py
@@ -1,6 +1,7 @@
 from unittest import TestCase, main
 
 from ugrd.initramfs_generator import InitramfsGenerator
+from ugrd.kmod import MissingModuleError
 from zenlib.logging import loggify
 
 
@@ -20,7 +21,10 @@ class TestCpio(TestCase):
 
     def test_f2fs(self):
         generator = InitramfsGenerator(logger=self.logger, config="tests/fs/f2fs.toml")
-        generator.build()
+        try:
+            generator.build()
+        except MissingModuleError:
+            generator.logger.critical("F2FS is not supported on this system, skipping test.")
 
     def test_overlayfs(self):
         generator = InitramfsGenerator(logger=self.logger, config="tests/fs/overlayfs.toml")
