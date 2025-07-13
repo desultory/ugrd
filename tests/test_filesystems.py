6ac846166a4845d7fd66a6fb019ac8da44ca7aeb
Zen <z@pyl.onl>
2025-07-13 13:27:38 -0500

    improve comments/naming for filesystem tests
    Signed-off-by: Zen <z@pyl.onl>

--- tests/test_filesystems.py
+++ tests/test_filesystems.py
@@ -6,20 +6,24 @@ from zenlib.logging import loggify
 
 
 @loggify
-class TestCpio(TestCase):
+class TestFilesystems(TestCase):
     def test_ext4(self):
+        """ Test ext4 root filesystem functionality. """
         generator = InitramfsGenerator(logger=self.logger, config="tests/fs/ext4.toml")
         generator.build()
 
     def test_btrfs(self):
+        """ Test btrfs root filesystem functionality. """
         generator = InitramfsGenerator(logger=self.logger, config="tests/fs/btrfs.toml")
         generator.build()
 
     def test_xfs(self):
+        """ Test xfs root filesystem functionality. """
         generator = InitramfsGenerator(logger=self.logger, config="tests/fs/xfs.toml")
         generator.build()
 
     def test_f2fs(self):
+        """ Test f2fs root filesystem functionality. """
         generator = InitramfsGenerator(logger=self.logger, config="tests/fs/f2fs.toml")
         try:
             generator.build()
@@ -27,10 +31,12 @@ class TestCpio(TestCase):
             generator.logger.critical("F2FS is not supported on this system, skipping test.")
 
     def test_overlayfs(self):
+        """ Test overlayfs/tmpfs overlay over root creation. """
         generator = InitramfsGenerator(logger=self.logger, config="tests/fs/overlayfs.toml")
         generator.build()
 
     def test_squashfs(self):
+        """ Test squashfs/overlayfs/tmpfs for live cd systems. """
         generator = InitramfsGenerator(logger=self.logger, config="tests/fs/squashfs.toml")
         generator.build()
 
