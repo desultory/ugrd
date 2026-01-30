434a3dbefd97d38fcb9a25644a58db4bbb9bb703
Zen <z@pyl.onl>
2026-01-30 17:44:39 -0600

    pull all udev rules, not just ones in /etc
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/base/udev.py
+++ src/ugrd/base/udev.py
@@ -1,21 +1,18 @@
-
 from pathlib import Path
 
 
 def pull_udev_deps(self):
-    """ Pulls udev rule files """
+    """Pulls udev rule files"""
 
-    udev_rule_dir = Path("/etc/udev/rules.d")
+    udev_rule_dirs = [Path("/etc/udev/rules.d"), Path("/lib/udev/rules.d")]
 
-    for rule_file in udev_rule_dir.glob("*.rules"):
-        self["dependencies"] = rule_file
+    for rule_dir in udev_rule_dirs:
+        for rule_file in rule_dir.glob("*.rules"):
+            self["dependencies"] = rule_file
 
 
 def start_udev(self):
-    """ Returns shell lines to start udev """
+    """Returns shell lines to start udev"""
     return """
     /lib/systemd/systemd-udevd --daemon
     udevadm trigger"""
-
-
-
