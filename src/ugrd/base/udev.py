92da3e694a3a890cf02ecd26248e296a8ff6728d
Zen <z@pyl.onl>
2026-01-30 18:18:50 -0600

    Dynamically find udev dependencies
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/base/udev.py
+++ src/ugrd/base/udev.py
@@ -1,5 +1,26 @@
 from pathlib import Path
 
+from zenlib.util import colorize as c_
+
+from ugrd.exceptions import AutodetectError
+
+
+def _get_udev_rule_progs(rule_file: Path):
+    """Extracts programs called by a udev rule file"""
+
+    progs = set()
+
+    with rule_file.open("r") as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith("#") or not line or "IMPORT{program}" not in line:
+                continue
+
+            progname = line[line.index("IMPORT{program}") + 17 :].split()[0].strip('";, ')
+            progs.add(progname)
+
+    return list(progs)
+
 
 def pull_udev_deps(self):
     """Pulls udev rule files"""
@@ -9,6 +30,14 @@ def pull_udev_deps(self):
     for rule_dir in udev_rule_dirs:
         for rule_file in rule_dir.glob("*.rules"):
             self["dependencies"] = rule_file
+            if progs := _get_udev_rule_progs(rule_file):
+                self.logger.info(
+                    f"[{c_(rule_file.name, 'blue')}] Found binary requirements: {c_(', '.join(progs), 'green')}"
+                )
+                try:
+                    self["binaries"] = progs
+                except AutodetectError as e:
+                    self.logger.warning(f"[{c_(rule_file.name, 'red')}] Unable to find udev rule dependency: {e}")
 
 
 def start_udev(self):
