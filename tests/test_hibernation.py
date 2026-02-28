287b5c7020889a9b6c72a9ee12d164d5e26cb6e9
Zen <z@pyl.onl>
2026-02-28 13:55:38 -0600

    add test for the resume module without actually hibernating
    also remove unused imports
    
    Signed-off-by: Zen <z@pyl.onl>

--- tests/test_hibernation.py
+++ tests/test_hibernation.py
@@ -1,8 +1,6 @@
 from unittest import TestCase, main
 
-from ugrd.exceptions import AutodetectError
 from ugrd.initramfs_generator import InitramfsGenerator
-from ugrd.kmod import MissingModuleError
 from zenlib.logging import loggify
 
 
@@ -12,6 +10,11 @@ class TestHibernation(TestCase):
         """Test swap partition hibernation functionality."""
         #generator = InitramfsGenerator(logger=self.logger, config="tests/fullauto.toml", test_swap_partition=True, test_kernel="/boot/vmlinuz-6.12.63-gentoo-dist", kernel_version="6.12.63-gentoo-dist")
         generator = InitramfsGenerator(logger=self.logger, config="tests/fullauto.toml", test_swap_partition=True)
+        generator.build()
+
+    def test_resume_no_hibernate(self):
+        """ Test with the resume module but without hibernating """
+        generator = InitramfsGenerator(logger=self.logger, config="tests/fullauto.toml", modules="ugrd.fs.resume")
         generator.build()
 
 if __name__ == "__main__":
