3725c581533cced34dd74a5ba8980437cd356378
Zen <z@pyl.onl>
2026-07-04 21:07:18 -0500

    fix black formatting in the bell module
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/base/bell.py
+++ src/ugrd/base/bell.py
@@ -3,21 +3,23 @@ __version__ = "0.1.1"
 
 
 def startup_bell(self) -> str:
-    """ Prints the bell symbol if the bell var is set """
+    """Prints the bell symbol if the bell var is set"""
     return """
     if check_var ugrd_bell; then
         printf '\a'
     fi
     """
 
+
 def end_bell(self) -> str:
-    """ Prints the bell symbol twice if the bell var is set """
+    """Prints the bell symbol twice if the bell var is set"""
     return """
     if check_var ugrd_bell; then
         printf '\a\a'
     fi
     """
 
+
 def export_bell(self) -> None:
-    """ Adds the bell variable to the exports dict """
+    """Adds the bell variable to the exports dict"""
     self["exports"]["ugrd_bell"] = 1 if self["bell"] else 0
