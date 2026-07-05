79b014c41116ef42364744d96f3d40626b326dfc
Zen <z@pyl.onl>
2026-07-04 22:01:20 -0500

    black format zfs module
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/fs/zfs.py
+++ src/ugrd/fs/zfs.py
@@ -2,7 +2,7 @@ __version__ = "0.2.2"
 
 
 def zpool_import(self) -> str:
-    """ Returns bash lines to import all ZFS pools """
+    """Returns bash lines to import all ZFS pools"""
     return """
     edebug 'Importing all ZFS pools'
     export ZPOOL_IMPORT_UDEV_TIMEOUT_MS=0  # Disable udev timeout
