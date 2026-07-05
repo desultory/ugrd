a82aa40ad4bc9b3770bf7a25f22e41326a710bb2
Zen <z@pyl.onl>
2026-07-05 18:33:08 -0500

    make initramfsprotocol an actual protocol
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/initramfs_protocol.py
+++ src/ugrd/initramfs_protocol.py
@@ -1,18 +1,18 @@
-from typing import Any
+from typing import Any, Protocol
 
 from zenlib.typing import HasLogger
 
 from ugrd.initramfs_dict import InitramfsConfigDict
 
 
-class InitramfsProtocol(HasLogger):
+class InitramfsProtocol(HasLogger, Protocol):
     config_dict: InitramfsConfigDict
     included_functions: dict[str, str | list[str]]
     build_tasks: list[str]
     init_types: list[str]
 
-
-    # Add definitions for functions defining dict like behavior
-    def __getattr__(self, item: str) -> Any: ...
-
+    # Add basic definitions for functions defining dict like behavior
+    def get(self, item: str, default: Any = None) -> Any: ...
     def __getitem__(self, item: str) -> Any: ...
+    def __setitem__(self, item: str, value: Any) -> None: ...
+    def __contains__(self, key: str) -> bool: ...
