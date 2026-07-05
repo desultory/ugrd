75d0737281953297f2411f04bbd036a467ab5e5d
Zen <z@pyl.onl>
2026-07-05 12:21:02 -0500

    improve typing in cryptographic modules
    Signed-off-by: Zen <z@pyl.onl>

--- src/ugrd/crypto/gpg.py
+++ src/ugrd/crypto/gpg.py
@@ -1,8 +1,10 @@
 __author__ = "desultory"
 __version__ = "1.1.4"
 
+from ugrd import InitramfsProtocol
 
-def start_agent(self) -> str:
+
+def start_agent(self: InitramfsProtocol) -> str:
     """Start the GPG agent."""
     args = (" " + " ".join(self["gpg_agent_args"])) if self["gpg_agent_args"] else ""
     return f'einfo "Starting GPG agent: $(gpg-agent{args} 2>&1)"'
